1116768a3fb6003397e4d1bb93f33afc6509ea0f
hennersz <henry@morti.net>
2021-03-05 15:38:41 +0000

    fix: escape space

--- home/role/darwin-laptop/default.nix
+++ home/role/darwin-laptop/default.nix
@@ -11,7 +11,7 @@ lib;
 
     home.activation = {
       codeUseConfDir = lib.hm.dag.entryAfter ["linkCode"] ''
-        $DRY_RUN_CMD ln -sf $VERBOSE_ARG ${config.xdg.configHome}/Code/User ~/Library/Application Support/Code
+        $DRY_RUN_CMD ln -sf $VERBOSE_ARG ${config.xdg.configHome}/Code/User ~/Library/Application\ Support/Code
       '';
     };
 
