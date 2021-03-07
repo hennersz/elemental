c584948b407fcdf46e9b46377256c1280bdd0b16
hennersz <henry@morti.net>
2021-03-07 21:20:04 +0000

    feat: add option for OSX >= 10.15 nix install

--- init.sh
+++ init.sh
@@ -1,8 +1,22 @@
-#! /bin/bash
+#!/bin/bash
 
 set -e
 
-curl -L https://nixos.org/nix/install | sh
+os=$(uname)
+if [ "$os" = "Darwin" ]; then 
+  . ./vercomp.sh
+  ver=$(sw_vers -productVersion)
+  vercomp "$ver" "10.14"
+  if [ $? = 1 ]; then 
+    sh <(curl -L https://nixos.org/nix/install) --darwin-use-unencrypted-nix-store-volume
+  else
+    curl -L https://nixos.org/nix/install | sh
+  fi
+else
+  curl -L https://nixos.org/nix/install | sh
+fi
+
+# shellcheck source=/dev/null
 . ~/.nix-profile/etc/profile.d/nix.sh
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
@@ -10,6 +24,6 @@ nix-channel --update
 nix-shell '<home-manager>' -A install
 home-manager switch
 
-sudo echo "/usr/local/bin/fish" >> /etc/shells
+echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
 chsh --shell /usr/local/bin/fish
 fish
