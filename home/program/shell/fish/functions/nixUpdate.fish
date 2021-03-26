836bb6069aabfc600cdddc7bb995c3131a3ae8eb
Henry Mortimer <henry@morti.net>
2021-03-26 16:17:37 +0000

    feat: recurse submodules when updating config

--- home/program/shell/fish/functions/nixUpdate.fish
+++ home/program/shell/fish/functions/nixUpdate.fish
@@ -1,7 +1,7 @@
 function nixUpdate
   set originalDir (pwd)
   cd ~/.config/nixpkgs
-  and git pull
+  and git pull --recurse-submodules
   and nix-channel --update
   and home-manager switch
   and cd $originalDir
