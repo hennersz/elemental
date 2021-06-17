131d86c76b8ae16be1aa7347adb28ad1885aec10
Henry Mortimer <henry@morti.net>
2021-06-17 13:13:33 +0100

    fix: fully update submodules on nixUpdate

--- home/program/shell/fish/functions/nixUpdate.fish
+++ home/program/shell/fish/functions/nixUpdate.fish
@@ -2,6 +2,7 @@ function nixUpdate
   set originalDir (pwd)
   cd ~/.config/nixpkgs
   and git pull --recurse-submodules
+  and git submodule update --remote --recursive
   and nix-channel --update
   and home-manager switch
   and cd $originalDir
