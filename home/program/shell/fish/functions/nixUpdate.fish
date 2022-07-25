0f8a692bf7bda9f91138bbe8f6d20186a7eabf87
Henry Mortimer <henry.mortimer@control-plane.io>
2022-07-25 13:43:39 +0100

    fix: remove submodule pull from update function

--- home/program/shell/fish/functions/nixUpdate.fish
+++ home/program/shell/fish/functions/nixUpdate.fish
@@ -2,7 +2,6 @@ function nixUpdate
   set originalDir (pwd)
   cd ~/.config/nixpkgs
   and git pull
-  and git submodule update --remote --recursive --init
   and nix-channel --update
   and home-manager switch
   and cd $originalDir
