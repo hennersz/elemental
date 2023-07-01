ba9e66228c14eab06dd50be32bb9f550e610a148
Henry Mortimer <henry@morti.net>
2023-07-01 12:05:11 +0100

    fix: allow incoming tailscale traffic

--- modules/nixos/mixins/vpn.nix
+++ modules/nixos/mixins/vpn.nix
@@ -3,4 +3,18 @@
   environment.systemPackages = [ pkgs.tailscale ];
   services.tailscale.enable = true;
   networking.firewall.checkReversePath = "loose";
+  networking.firewall = {
+    # enable the firewall
+    enable = true;
+
+    # always allow traffic from your Tailscale network
+    trustedInterfaces = [ "tailscale0" ];
+
+    # allow the Tailscale UDP port through the firewall
+    allowedUDPPorts = [ config.services.tailscale.port ];
+
+    # allow you to SSH in over the public internet
+    networking.firewall.allowedTCPPorts = [ 22 ];
+  };
+
 }
