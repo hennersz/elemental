69b61730d6cd28857dbcea92f16c819c4988dd33
Henry Mortimer <henry.mortimer@control-plane.io>
2024-01-22 18:08:42 +0000

    feat: updgrade alacritty config

--- modules/homeManager/program/terminal/alacritty/default.nix
+++ modules/homeManager/program/terminal/alacritty/default.nix
@@ -3,18 +3,48 @@ with
 lib;
 let
   cfg = config.elemental.home.program.terminal.alacritty;
+  tomlFormat = pkgs.formats.toml { };
+  configFileName = "alacritty.toml";
   themes = pkgs.fetchFromGitHub {
     owner = "alacritty";
     repo = "alacritty-theme";
-    rev = "e4464d5ed853d510db5d93b5199d4569d34669d8";
-    sha256 = "C0q4eBtxpER2ellG0Os/BTL3MDaq4rao3Yh+ytnR1X4=";
+    rev = "f03686afad05274f5fbd2507f85f95b1a6542df4";
+    sha256 = "457kKE3I4zGf1EKkEoyZu0Fa/1O3yiryzHVEw2rNZt8=";
+  };
+
+  defaultSettings = {
+    env = {
+      "TERM" = "xterm-256color";
+      "LC_ALL" = "";
+    };
+
+    import = [
+      "${themes}/themes/solarized_light.toml"
+    ];
+
+    window = {
+      padding.x = 0;
+      padding.y = 10;
+      decorations = "full";
+      opacity = 1;
+    };
+
+    font = {
+      size = 14.0;
+
+      normal.family = "SpaceMono Nerd Font";
+      bold.family = "SpaceMono Nerd Font";
+      italic.family = "SpaceMono Nerd Font";
+    };
+
+    cursor.style = "Underline";
   };
 in
 {
   options.elemental.home.program.terminal.alacritty = {
     enable = mkEnableOption "Enable the alacritty terminal";
 
-    settingOverrides = mkOption {
+    settingsOverrides = mkOption {
       type = types.attrs;
       default = { };
       description = "Override the elemental default config";
@@ -22,37 +52,18 @@ in
   };
 
   config = mkIf cfg.enable {
-    programs.alacritty = {
-      enable = true;
-      settings = lib.attrsets.recursiveUpdate
-        {
-          env = {
-            "TERM" = "xterm-256color";
-            "LC_ALL" = "";
-          };
+    home.packages = mkIf (config.elemental.role == "nixos-workstation") [ pkgs.alacritty ];
 
-          import = [
-            "${themes}/themes/solarized_light.yaml"
-          ];
-
-          window = {
-            padding.x = 0;
-            padding.y = 10;
-            decorations = "full";
-            opacity = 1;
-          };
-
-          font = {
-            size = 14.0;
-
-            normal.family = "SpaceMono Nerd Font";
-            bold.family = "SpaceMono Nerd Font";
-            italic.family = "SpaceMono Nerd Font";
-          };
-
-          cursor.style = "Underline";
-        }
-        cfg.settingOverrides;
+    xdg.configFile."alacritty/${configFileName}" = {
+      source =
+        (tomlFormat.generate configFileName (lib.attrsets.recursiveUpdate defaultSettings cfg.settingsOverrides)).overrideAttrs
+          (finalAttrs: prevAttrs: {
+            buildCommand = lib.concatStringsSep "\n" [
+              prevAttrs.buildCommand
+              # TODO: why is this needed? Is there a better way to retain escape sequences?
+              "substituteInPlace $out --replace '\\\\' '\\'"
+            ];
+          });
     };
   };
 }
