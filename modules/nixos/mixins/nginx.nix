b0208736fe3c58d028f7f3ad046a430e651de9b9
Henry Mortimer <henry@morti.net>
2024-02-19 18:00:24 +0000

    feat: send nginx logs to stdout

--- modules/nixos/mixins/nginx.nix
+++ modules/nixos/mixins/nginx.nix
@@ -2,5 +2,8 @@
 {
   services.nginx.enable = true;
   services.nginx.recommendedProxySettings = true;
+  services.nginx.appendHttpConfig = ''
+    access_log syslog:server=unix:/dev/log combined;
+  '';
   networking.firewall.allowedTCPPorts = [ 80 443 ];
 }
