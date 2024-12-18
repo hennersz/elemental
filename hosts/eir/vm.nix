2b6240c5d521ddaaab71e2798a34f27b37102a1e
Henry Mortimer <henry@morti.net>
2024-12-18 21:37:30 +0000

    fixes

--- hosts/eir/vm.nix
+++ hosts/eir/vm.nix
@@ -22,8 +22,7 @@
   elemental.pi-hole = {
     interfaces = [ "eth0" "tailscale0" ];
     domain = config.networking.hostName;
-    hostIP = lib.mkIf (lib.strings.hasSuffix "darwin" hostSystem) "192.168.105.101";
-    hostIP = lib.mkIf !(lib.strings.hasSuffix "darwin" hostSystem) "10.0.0.2";
+    hostIP = if (lib.strings.hasSuffix "darwin" hostSystem) then "192.168.105.101" else "10.0.0.2";
   };
   system.activationScripts.makeAppDataDir = lib.stringAfter [ "var" ] ''
     mkdir -p /var/lib/app-data
