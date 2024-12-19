8a98d99d844acedf6b4dbdbc6c9970292e7bfd3c
Henry Mortimer <henry@morti.net>
2024-12-19 18:41:58 +0000

    chore: formatting

--- utils.nix
+++ utils.nix
@@ -16,7 +16,7 @@
 
   mkPkgs = { system, nixpkgs ? inputs.nixpkgs-unstable }: import nixpkgs {
     inherit system;
-    config.allowUnfree = true;    
+    config.allowUnfree = true;
     config.permittedInsecurePackages = [
       "electron-27.3.11"
     ];
