09f75cab8651a74f4de66ce24f9c7acf404c5662
Henry Mortimer <henry.mortimer@neo4j.com>
2025-08-21 14:03:56 +0100

    fix: set primary user on baldur

--- hosts/baldur/configuration.nix
+++ hosts/baldur/configuration.nix
@@ -62,6 +62,7 @@
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
+  system.primaryUser = "henry";
 
   # The platform the configuration will be used on.
   nixpkgs.hostPlatform = "aarch64-darwin";
