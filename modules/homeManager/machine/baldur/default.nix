1e0ddd074ad427c9393ddd409a8575fd00110a00
Henry Mortimer <henry@morti.net>
2026-08-06 16:12:48 +0100

    feat: add node to baldur

--- modules/homeManager/machine/baldur/default.nix
+++ modules/homeManager/machine/baldur/default.nix
@@ -1,7 +1,12 @@
-{ config, lib, pkgs, ... }:
+{ config
+, lib
+, pkgs
+, ...
+}:
 {
   config = lib.mkIf (config.elemental.machine == "baldur") {
-    elemental.home.program.editor.neovim.anthropicApiKeyRef = "op://Private/Anthropic API Key/credential";
+    elemental.home.program.editor.neovim.anthropicApiKeyRef =
+      "op://Private/Anthropic API Key/credential";
 
     programs.password-store = {
       enable = true;
@@ -12,6 +17,7 @@
 
     home.packages = with pkgs; [
       zed-editor
+      nodejs
     ];
   };
 }
